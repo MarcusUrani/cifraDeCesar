6b9e946cec151de9e21294b2522c0c6970c7a780
Marcus <marcus.urani20@gmail.com>
2022-10-04 15:28:58 -0300

    Fazendo últimos ajustes e compilando

--- cifraDeCesar.c
+++ cifraDeCesar.c
@@ -6,12 +6,12 @@ int main(void)
     char text[82];
     scanf("%d %d", &k, &n);
     getchar();
-    for (i = 0; i < n; i++)
+    for (i = 0; i < k; i++)
     {
         fgets(text, 82, stdin);
         for (j = 0; text[j] != '\n'; j++)
         {
-            int c = text[j] + k;
+            int c = text[j] + n;
             if (c > 126)
             {
                 c -= 95;
