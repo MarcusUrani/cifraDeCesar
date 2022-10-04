ed06b0033967906eb5b38ade7c2561f610c0ca85
Marcus <marcus.urani20@gmail.com>
2022-10-04 15:24:01 -0300

    Criando o loop

--- cifraDeCesar.c
+++ cifraDeCesar.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
     int i, j, k, n;
     char text[82];
-
     scanf("%d %d", &k, &n);
+    getchar();
+    for (i = 0; i < n; i++)
+    {
+    }
+
     return 0;
 }
